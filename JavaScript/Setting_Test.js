a1564e0f1e3d6329467ffbeb421bed3089341db9
Mister-Phil <72716392+Mister-Phil@users.noreply.github.com>
2022-12-28 23:50:54 -0500

    New files
    Adding new test functions here.

---
+++ JavaScript/Setting_Test.js
@@ -0,0 +1,61 @@
+/*
+Running tests in Visual Studio Code
+You can run tests in visual studio code in a couple of ways:
+
+Running the .Tests.ps1 script from the Debug menu. Keyboard short cuts are: F5 or Shift + F5
+From within the editor by clicking on the "Run Tests" link above the Describe keyword in the test code itself
+Configuring a Task to run the Pester framework
+*/
+
+
+var hello = "Hello World!";
+console.log("I want to say. " + hello);
+
+const soldierA =
+{   //Soldier1
+    codeName: "X-phantom", 
+    rName: 'Bryan Joe',
+    birth: 06-02-1992,
+    rank: 9000,
+    weapon: "Eagle",
+};
+const soldierB =
+{   //Soldier2
+    codeName: "Lily Death", 
+    rName: 'Hanna Torn',
+    birth: 06-02-1994,
+    rank: 7000,
+    weapon: "PP6",
+};
+
+
+console.log("His real name is: " + soldierA.rName + "; his best weapon is " + soldierA.weapon);
+console.log("For her, she is: " + soldierB.rName + "; her fav weapon is the " + soldierB.weapon);
+
+
+/*
+var hello = "Hello World!";
+console.log("I want to say. " + hello);
+
+const soldiers = [
+{   //Soldier1
+    ID: soldierA,
+    codeName: "X-phantom", 
+    rName: 'Bryan Joe',
+    birth: 06-02-1992,
+    rank: 9000,
+    weapon: "Eagle",
+},
+{   //Soldier2
+    ID: soldierB,
+    codeName: "Lily Death", 
+    rName: 'Hanna Torn',
+    birth: 06-02-1994,
+    rank: 7000,
+    weapon: "PP6",
+};
+];//End soldiers list.
+
+console.log("His real name is: " + soldierA.rName + "; his best weapon is " + soldierA.weapon);
+console.log("For her, she is: " + soldierB.rName + "; her fav weapon is the " + soldierB.weapon);
+*/
